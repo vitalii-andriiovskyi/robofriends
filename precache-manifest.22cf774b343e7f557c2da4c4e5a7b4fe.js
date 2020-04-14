71b144270141fe869abe244302a907be8ff0d0e9
Vitalii Andriiovskyi <domovik0712@gmail.com>
2020-04-14 18:14:00 +0300

    Updates

---
+++ precache-manifest.22cf774b343e7f557c2da4c4e5a7b4fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672527ed5ea14a2780dc27bded41054a",
+    "url": "/vitalii-andriiovskyi/robofriens/index.html"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/vitalii-andriiovskyi/robofriens/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "69cec3036a1fbc401220",
+    "url": "/vitalii-andriiovskyi/robofriens/static/css/main.c1c4d458.chunk.css"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/vitalii-andriiovskyi/robofriens/static/js/2.e636b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vitalii-andriiovskyi/robofriens/static/js/2.e636b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69cec3036a1fbc401220",
+    "url": "/vitalii-andriiovskyi/robofriens/static/js/main.e4a37881.chunk.js"
+  },
+  {
+    "revision": "d492f774deb3f6f74469",
+    "url": "/vitalii-andriiovskyi/robofriens/static/js/runtime-main.a230aa32.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/vitalii-andriiovskyi/robofriens/static/media/SEGA.12a3db8b.woff"
+  }
+]);
