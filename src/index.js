eb05ecf01da5276bf927571521bff9572d0f3d50
Vitalii Andriiovskyi <domovik0712@gmail.com>
2020-04-16 18:04:11 +0300

    feat: rootReducer, thunkMiddleware

--- src/index.js
+++ src/index.js
@@ -7,17 +7,19 @@ import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 import { Provider } from 'react-redux';
-import {
-  createStore,
-  // applyMiddleware 
-} from 'redux';
-// import { logger } from 'redux-logger';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { logger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducers';
+
+const rootReducer = combineReducers({
+  searchRobots,
+  requestRobots
+})
 
 const store = createStore(
-  searchRobots,
-  // applyMiddleware(logger)
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger),
 );
 
 ReactDOM.render(
